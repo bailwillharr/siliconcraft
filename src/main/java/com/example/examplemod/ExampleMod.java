2da3304e6dc6bf17106d2c8c216c4ad955b50947
Bailey Harrison <baileyharrison04@outlook.com>
2019-07-19 10:01:21 +0100

    Update ExampleMod.java

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,20 +1,20 @@
-package com.example.examplemod;
+package bailwillharr.siliconcraft;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = SiliconCraft.MODID, version = SiliconCraft.VERSION)
+public class SiliconCraft
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "siliconcraft";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
 		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        System.out.println("Loading SiliconCraft...");
     }
 }
